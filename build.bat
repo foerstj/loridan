15f3286077c8e315335167a8c56ae38dca6c2e15
Johannes Förstner <johannes@famfoerstner.de>
2023-02-22 18:39:31 +0100

    Add fix_start_positions_required_levels on build

--- build.bat
+++ build.bat
@@ -4,18 +4,24 @@
 :: name of map
 set map=loridan1.5-loa
 :: name of map, case-sensitive
-set map_cs=Loridan
+set map_cs=Loridan1.5Loa
 :: path of DSLOA documents dir (where Bits are)
 set doc_dsloa=%USERPROFILE%\Documents\Dungeon Siege LoA
 :: path of DS installation
 set ds=.
 :: path of TankCreator
 set tc=..\TankCreator
+:: path of GasPy
+set gaspy=..\gaspy
 
 :: Compile map file
 rmdir /S /Q "%tmp%\Bits"
 robocopy "%doc_dsloa%\Bits\world\maps\%map%" "%tmp%\Bits\world\maps\%map%" /E
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\Maps\%map_cs%.dsmap" -copyright "Minotaurus 2006" -title "%map_cs%" -author "Johannes Förstner"
+pushd %gaspy%
+venv\Scripts\python -m build.fix_start_positions_required_levels %map% "%tmp%\Bits"
+if %errorlevel% neq 0 pause
+popd
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.dsmap" -copyright "Minotaurus 2006" -title "%map_cs%" -author "Johannes Förstner"
 if %errorlevel% neq 0 pause
 
 :: Cleanup
