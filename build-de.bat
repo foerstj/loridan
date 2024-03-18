3bb01d5415fa125e33e48af9d211cb41c797efac
Johannes Förstner <johannes@famfoerstner.de>
2024-03-18 18:19:07 +0100

    Simplify vars, stick to my naming convention

--- build-de.bat
+++ build-de.bat
@@ -1,16 +1,5 @@
-:: name of map, case-sensitive
-set map_cs=Loridan
-:: namespace of resources
-set res=loridan
-:: path of DSLOA documents dir (where Bits are)
-set doc_dsloa=%USERPROFILE%\Documents\Dungeon Siege LoA
-:: path of DS installation
-set ds=.
-:: path of TankCreator
-set tc=..\TankCreator
-
 :: Compile German language resource file
 rmdir /S /Q "%tmp%\Bits"
-robocopy "%doc_dsloa%\Bits\language" "%tmp%\Bits\language" %res%-* /S
-%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%-de.dsres" -copyright "CC-BY-SA 2023" -title "%map_cs%" -author "Johannes Förstner"
+robocopy "%doc_dsloa%\Bits\language" "%tmp%\Bits\language" %map%-*.de.gas /S
+%tc%\RTC.exe -source "%tmp%\Bits" -out "%ds%\DSLOA\%map_cs%.de.dsres" -copyright "CC-BY-SA 2023" -title "%map_cs%" -author "Johannes Förstner"
 if %errorlevel% neq 0 pause
